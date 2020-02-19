0909e5c9a2b5d56a1cf01338c98634b55faea9d2
Gustavo Rigor <37718815+GusRigor@users.noreply.github.com>
2020-02-18 22:18:57 -0300

    Update main.cpp
    Atualizando com exponencial

--- exemplo1/main.cpp
+++ exemplo1/main.cpp
@@ -18,11 +18,24 @@ unsigned long long fatorial( int n )
       i += ((pow(-1,i))/fatorial((2*i)+1))*(pow(x,((2*i+1))));
     return i;
   }
+  double expo(double x){
+    
+    return 1 + (pow(x,1))/fatorial(1);
+  }
+  double expo10(double x){
+    double j = 0;
+    for(int i=0; i<10; i++){
+      j += (pow(x,i))/fatorial(i);
+    }
+    return j;
+  }
 
 int main() {
   int x = 2;
   x = pow(2,2);
   double y = fatorial(4);
   double y1 = seno10((3.14159265359)/6);
+  y = expo(1);
+  y1 = expo10(1);
   std::cout<< y <<"\n"<<y1;
 }
