41c96fac98b0eece56f0ddeca3aef1d025f7b574
Gustavo Rigor <37718815+GusRigor@users.noreply.github.com>
2020-02-18 22:09:12 -0300

    Update main.cpp
    apenas atualizando

--- exemplo1/main.cpp
+++ exemplo1/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
-#include <stdlib.h>
+#include <math.h>
+unsigned long long fatorial( int n )
+{
+    unsigned long long f = 1;
+    for( ; n > 0; f *= n-- );
+    return f;
+}
+
+  double seno(double x){
+    double i = ((pow(-1,1))/fatorial((2*1)+1))*(pow(x,((2*1+1))));
+    return i;
+  }
+
+  double seno10(double x){
+    double i;
+    for(int j=0; j<10;j++)
+      i += ((pow(-1,i))/fatorial((2*i)+1))*(pow(x,((2*i+1))));
+    return i;
+  }
 
 int main() {
-  std::cout << "Hello World!\n";
-  char c[5];
-  double pi = 3.14;
-
-  c = string(pi);
+  int x = 2;
+  x = pow(2,2);
+  double y = fatorial(4);
+  double y1 = seno10((3.14159265359)/6);
+  std::cout<< y <<"\n"<<y1;
+}
