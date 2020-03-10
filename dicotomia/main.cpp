a6f5bf65ec6c0d497642df45a8f89ad515738853
Gustavo Rigor <37718815+GusRigor@users.noreply.github.com>
2020-03-10 10:13:49 -0300

    Update main.cpp

--- dicotomia/main.cpp
+++ dicotomia/main.cpp
@@ -1,12 +1,5 @@
 #include <iostream>
-double max(double x[], int n);
-//Para os outros Ex, basta ajustar a funcao calc
-double calc(double x);
-double erro(double x, double y);
-double xTil(double x, double y);
-double mod(double x);
-double fxfa(double x, double y);
-
+#include "calc.h"
 
 int main() {
   int iA=0;
@@ -36,40 +29,5 @@ int main() {
   }while(mod(x[iX-1]-x[iX-2]) > (e*max(x,(iX-1))));
  
 
-  std::cout << "Valor da raiz: "<<x[iX-1]<< " ± "<<e;
-}
-
-double max(double x[], int n){
-  double temp=0;
-  for(int i=0;i<n;i++){
-    if(x[i]>temp){
-      temp=x[i];
-    }
-  }
-  return temp;
-}
-
-double calc(double x){
-  return (x*x+(0.96*x)-2.08);
-}
-
-double erro(double x, double y){
-  return (y-x)/2;
-}
-
-double xTil(double x, double y){
-  return (y+x)/2;
-}
-
-double mod(double x){
-  if(x<0)
-    return x*(-1);
-  return x;
-}
-
-double fxfa(double x, double y){
-  double temp = calc(x);
-  double temp1 = calc(y);
-
-  return temp*temp1;
+  std::cout << "Valor da raiz\nExercício A: "<<x[iX-1]<< " ± "<<e<<"\n";
 }
