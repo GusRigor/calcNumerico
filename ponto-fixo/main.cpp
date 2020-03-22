6842db559b6ee1a5be558731fdb36ec811daf2cc
Gustavo <gusrigor@icloud.com>
2020-03-22 12:26:22 -0300

    adicionandas as funcoes fi e dfi

--- ponto-fixo/main.cpp
+++ ponto-fixo/main.cpp
@@ -1,6 +1,36 @@
+/*
+Método do ponto fixo
+Feito em função dos exercícios da
+segunda aula do curso de calcNumérico
+Por: Gustavo G. Rigor
+*/
 #include <iostream>
 #include <math.h>
+double max(double x[], int n);
+double fi(double x);
+double dfi(double x);
 
-void main() {
 
+int main() {
+	double resultado = 0;
+
+	return 0;
+}
+
+double fi(double x) {
+    return (2.08 - 0.96 * x) / x;
+}
+
+double dfi(double x) {
+    return (-2.08 / (x * x));
+}
+
+double max(double x[], int n) {
+    double temp = 0;
+    for (int i = 0; i < n; i++) {
+        if (x[i] > temp) {
+            temp = x[i];
+        }
+    }
+    return temp;
 }
