dde00e81b1645fa40aa0292f598960944bb269b0
Gustavo <gusrigor@icloud.com>
2020-03-23 11:03:36 -0300

    adicionado o metodo de newton

---
+++ newton-rapson/main.cpp
@@ -0,0 +1,32 @@
+/*
+Método Newton Rapson
+Feito em função dos exercícios da
+segunda aula do curso de calcNumérico
+Por: Gustavo G. Rigor
+*/
+#include <iostream>
+#include <math.h>
+
+double f(double x);
+double df(double x);
+double mod(double x);
+
+int main() {
+
+
+	return 0;
+}
+
+double f(double x) {
+    return (x*x*x*x)-2.0;
+}
+
+double df(double x) {
+    return (x * x * x)*4;
+}
+
+double mod(double x) {
+    if (x < 0)
+        return x * (-1);
+    return x;
+}
