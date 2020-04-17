c30a99e7aa380f1b0a1990395965a5710306ccfe
gustavo rigor <gusrigor@icloud.com>
2020-04-17 01:30:34 -0300

    Criado o programa para o cálculo da trajetória

---
+++ Edo 1/ExFuzil/main.py
@@ -0,0 +1,8 @@
+#Este é um programa para calcular a trajetória de uma bala de um fuzil
+#Exercício da aula de cálculo numérico ministrada no quinto semestre de
+#Engenharia da Computação. Feito por Gustavo Gomes Rigor.
+#O fuzil escolhido foi AK47
+#Cartucho 5.45x39mm
+
+print("""O fuzil selecionado foi o AK-47
+utilizando o cartucho 5.45x39mm""")
