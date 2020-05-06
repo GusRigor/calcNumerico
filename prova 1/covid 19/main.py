387cc8de7f413c73c12c0798b656c0fc34b4a4e0
gustavo rigor <gusrigor@icloud.com>
2020-05-06 18:59:22 -0300

    atualizando

--- prova 1/covid 19/main.py
+++ prova 1/covid 19/main.py
@@ -6,6 +6,55 @@ import csv
 import math
 import matplotlib.pyplot
 
+def kUm1(x1):
+    return - k12 * x1
+
+def kDois1(x1, k1):
+    return - k12 * (x1 + k1/2)
+
+def kTres1(x1, k2):
+    return - k12 * (x1 + k2/2)
+
+def kQuatro1(x1, k3):
+    return - k12 * (x1 + k3)
+
+def kUm2(x1, x2):
+    return k12 * x1 - (k23 + k24) * x2
+
+def kDois2(x1, x2, k1):
+    return k12 * (x1 + k1 / 2) - (k23 + k24) * (x2 + k1 / 2)
+
+def kTres2(x1, x2, k2):
+    return k12 * (x1 + k2 / 2) - (k23 + k24) * (x2 + k2 / 2)
+
+def kQuatro2(x1, x2, k3):
+    return k12 * (x1 + k3) - (k23 + k24) * (x2 + k3)
+
+def kUm3(x2):
+    return k23 * x2
+    
+def kDois3(x3, k1):
+    return k23 * (x2 + k1/2)
+
+def kTres3(x2, k2):
+    return k23 * (x2 + k2/2)
+
+def kQuatro3(x2, k3):
+    return k23 * (x2 + k3)
+
+def kUm4(x2):
+    return k24 * x2
+    
+def kDois4(x2, k1):
+    return k24 * (x2 + k1/2)
+
+def kTres4(x2, k2):
+    return k24 * (x2 + k2/2)
+
+def kQuatro4(x2, k3):
+    return k24 * (x2 + k3)
+
+
 def DeltaX(caso):
     return int(caso[len(caso)-2]) - int(caso[len(caso)-1])
 
@@ -88,8 +137,48 @@ Brasil = 209000000 * 0.7 - int(casos[len(casos)-1])
 print(k12)
 print(k23)
 print(k24)
-print(f'{Brasil} + Pessoas')
 
+print(f'{Brasil:} Pessoas')
 
+h = 1
+t = 0
+
+rk11 = 0
+rk12 = 0
+rk13 = 0
+rk14 = 0
+
+rk21 = 0
+rk22 = 0
+rk23 = 0
+rk24 = 0
+
+rk31 = 0
+rk32 = 0
+rk33 = 0
+rk34 = 0
+
+rk41 = 0
+rk42 = 0
+rk43 = 0
+rk44 = 0
+
+x1 = Brasil
+x2 = int(casos[len(casos)-1])
+x3 = int(mortos[len(mortos)-1])
+x4 = int(recuperados[len(recuperados)-1])
+
+auxX1 = 0
+auxX2 = 0
+auxX3 = 0
+auxX4 = 0
+erro = 0
+
+matplotlib.pyplot.ioff()
+
+graphT = []
+graphX2 = []
+graphX3 = []
+graphX4 = []
 
 
