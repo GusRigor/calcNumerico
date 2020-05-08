9ef39e4015dc2cbfc4c725755903f359ca11f147
gustavo rigor <gusrigor@icloud.com>
2020-05-08 16:37:40 -0300

    Atualizando e encerrando

--- prova 1/covid 19/main sem.py
+++ prova 1/covid 19/main sem.py
@@ -6,29 +6,29 @@ import csv
 import math
 import matplotlib.pyplot
 
-def kUm1(x1):
-    return -1 * k12 * x1
+def kUm1(x1, x2):
+    return k21 * x2- k12 * x1
 
-def kDois1(x1, k1):
-    return -1 * k12 * (x1 + k1/2)
+def kDois1(x1, x2, k1):
+    return k21 * (x2 + k1/2)- k12 * (x1 + k1/2)
 
-def kTres1(x1, k2):
-    return -1 * k12 * (x1 + k2/2)
+def kTres1(x1, x2, k2):
+    return k21 * (x2 + k2/2)- k12 * (x1 + k2/2)
 
-def kQuatro1(x1, k3):
-    return -1 * k12 * (x1 + k3)
+def kQuatro1(x1,  x2, k3):
+    return k21 * (x2 + k3)- k12 * (x1 + k3)
 
 def kUm2(x1, x2):
-    return k12 * x1 - (k23 + k24) * x2
+    return k12 * x1 - (k23 + k21) * x2
 
 def kDois2(x1, x2, k1):
-    return k12 * (x1 + k1 / 2) - (k23 + k24) * (x2 + k1 / 2)
+    return k12 * (x1 + k1 / 2) - (k23 + k21) * (x2 + k1 / 2)
 
 def kTres2(x1, x2, k2):
-    return k12 * (x1 + k2 / 2) - (k23 + k24) * (x2 + k2 / 2)
+    return k12 * (x1 + k2 / 2) - (k23 + k21) * (x2 + k2 / 2)
 
 def kQuatro2(x1, x2, k3):
-    return k12 * (x1 + k3) - (k23 + k24) * (x2 + k3)
+    return k12 * (x1 + k3) - (k23 + k21) * (x2 + k3)
 
 def kUm3(x2):
     return k23 * x2
@@ -42,17 +42,6 @@ def kTres3(x2, k2):
 def kQuatro3(x2, k3):
     return k23 * (x2 + k3)
 
-def kUm4(x2):
-    return k24 * x2
-    
-def kDois4(x2, k1):
-    return k24 * (x2 + k1/2)
-
-def kTres4(x2, k2):
-    return k24 * (x2 + k2/2)
-
-def kQuatro4(x2, k3):
-    return k24 * (x2 + k3)
 
 
 def DeltaX(caso):
@@ -132,13 +121,13 @@ deltaRecuperados = DeltaX(recuperados)
 
 k12 = deltaCasos /  Brasil
 k23 = deltaMortes / int(casos[len(casos)-2])
-k24 = deltaRecuperados / int(casos[len(casos)-2])
+k21 = deltaRecuperados / int(casos[len(casos)-2])
 
 
 
 print(k12)
 print(k23)
-print(k24)
+print(k21)
 
 print(f'{Brasil:} Pessoas')
 
@@ -160,20 +149,14 @@ rk32 = 0
 rk33 = 0
 rk34 = 0
 
-rk41 = 0
-rk42 = 0
-rk43 = 0
-rk44 = 0
 
 x1 = Brasil
 x2 = int(casos[len(casos)-1])
 x3 = int(mortos[len(mortos)-1])
-x4 = int(recuperados[len(recuperados)-1])
 
 auxX1 = 0
 auxX2 = 0
 auxX3 = 0
-auxX4 = 0
 erro = 0
 
 matplotlib.pyplot.ioff()
@@ -181,61 +164,52 @@ matplotlib.pyplot.ioff()
 graphT = []
 graphX2 = []
 graphX3 = []
-graphX4 = []
 
 while t < 14:
     auxX1 = x1
     auxX2 = x2
     auxX3 = x3
-    auxX4 = x4
 
-    rk11 = h*kUm1(x1)
+    rk11 = h*kUm1(x1, x2)
     rk21 = h*kUm2(x1, x2)
     rk31 = h*kUm3(x2)
-    rk41 = h*kUm4(x2)
 
-    rk12 = h*kDois1(x1, rk11)
+    rk12 = h*kDois1(x1, x2, rk11)
     rk22 = h*kDois2(x1, x2, rk21)
     rk32 = h*kDois3(x2, rk31)
-    rk42 = h*kDois4(x2, rk41)
 
-    rk13 = h*kTres1(x1, rk12)
+    rk13 = h*kTres1(x1, x2, rk12)
     rk23 = h*kTres2(x1, x2, rk22)
     rk33 = h*kTres3(x2, rk32)
-    rk43 = h*kTres4(x2, rk42)
 
-    rk14 = h*kQuatro1(x1, rk13)
+    rk14 = h*kQuatro1(x1, x2, rk13)
     rk24 = h*kQuatro2(x1, x2, rk23)
     rk34 = h*kQuatro3(x2, rk33)
-    rk34 = h*kQuatro4(x2, rk43)
 
-    print(f't:{t} || PP:{x1:.0f} || C:{x2:.0f} || M:{x3:.0f} || R:{x4:.0f}')
+    print(f't:{t} || PP:{x1:.0f} || C:{x2:.0f} || M:{x3:.0f}')
 
     graphT.append(t)
     graphX2.append(x2)
     graphX3.append(x3)
-    graphX4.append(x4)
 
     x1 = x1 + (rk11 + 2 * rk12 + 2 * rk13 + rk14)/6
     x2 = x2 + (rk21 + 2 * rk22 + 2 * rk23 + rk24)/6
     x3 = x3 + (rk31 + 2 * rk32 + 2 * rk33 + rk34)/6
-    x4 = x4 + (rk41 + 2 * rk42 + 2 * rk43 + rk44)/6
 
     k12 = (x2 - auxX2) / x1
     print(f'X2: {x2}')
     print(f'auxX2: {auxX2}')
     print(f'K12: {k12}')
     k23 = (x3 - auxX3) / auxX2
-    k24 = (x4 - auxX4) / auxX2
+    k21 = (1 - x2/auxX2 - k23 + k12*x1/auxX2) / auxX2
 
-    erro = (x1 - auxX1) / auxX1 + (x2 - auxX2) / auxX2 + (x3 - auxX3) / auxX3 + (x4 - auxX4) / auxX4
+    erro = (x1 - auxX1) / auxX1 + (x2 - auxX2) / auxX2 + (x3 - auxX3) / auxX3
     t = t + h
 
 matplotlib.pyplot.plot(graphT,graphX2,label='C(t)')
 matplotlib.pyplot.plot(graphT,graphX3,label='M(t)')
-matplotlib.pyplot.plot(graphT,graphX4,label='R(t)')
 matplotlib.pyplot.xlabel('tempo (t)')
-matplotlib.pyplot.ylabel('valores (C(t) e M(t) e R(t))')
+matplotlib.pyplot.ylabel('valores (C(t) e M(t)')
 matplotlib.pyplot.title('Exercício Covid 19 graph - Gustavo R. BEC')
 matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
