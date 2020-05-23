febdaf7a6eb132d6839aa91aa429a6d58934c76f
gustavo rigor <gusrigor@icloud.com>
2020-05-22 22:49:02 -0300

    Update main.py

--- prova 2/neuronio/main.py
+++ prova 2/neuronio/main.py
@@ -31,100 +31,105 @@ def dMdT(m, v, t = 0.01 * 10**(-3)):
 def dHdT(h, v, t = 0.01 * 10**(-3)):
     return h + t * (alphaH(v) * (1 - h) - betaH(v) * h)
 
+again = 'S'
 
-print('''Modelo de Hodgkin e Huxlei
+while again == 'S': 
+    print('''Modelo de Hodgkin e Huxlei
 Feito por: Gustavo G. Rigor
 Onde é utilizado ∆t = 0.01 ms
 Dentro do intervalo de [0, 0.25] ms
 Caso deseje alterar ∆t aparte S''')
-AAA = input()
+    AAA = input()
 
 
-gK = 3.60
-gNa = 12.0
-gL = 0.03
+    gK = 3.60
+    gNa = 12.0
+    gL = 0.03
 
-vK = -77 * 10**(-3) #mV
-vNa = 50 * 10**(-3) #mV
-vL = -54.402 * 10**(-3) #mV
+    vK = -77 * 10**(-3) #mV
+    vNa = 50 * 10**(-3) #mV
+    vL = -54.402 * 10**(-3) #mV
 
-n0 = 0.3176
-m0 = 0.0529
-h0 = 0.5961
+    n0 = 0.3176
+    m0 = 0.0529
+    h0 = 0.5961
 
-auxN = n0
-auxM = m0
-auxH = h0
+    auxN = n0
+    auxM = m0
+    auxH = h0
 
-v0 = -65.002  * 10**(-3) #mV
-t0 = 0 #segundos
+    v0 = -65.002  * 10**(-3) #mV
+    t0 = 0 #segundos
 
-if AAA == 'S':
-    dT = float(input('Valor para ∆t em ms')) * 10**(-3) #ms
-else:    
-    dT = 0.01 * 10**(-3) #ms
-Cm = 1 * 10**(-6) #µF
-gama = dT/Cm
+    if AAA == 'S':
+        dT = float(input('Valor para ∆t em ms')) * 10**(-3) #ms
+    else:    
+        dT = 0.01 * 10**(-3) #ms
+    Cm = 1 * 10**(-6) #µF
+    gama = dT/Cm
 
-tensao = []
-tempo = []
-tempo.append(t0)
-tensao.append(v0)
+    tensao = []
+    tempo = []
+    tempo.append(t0)
+    tensao.append(v0)
 
-pN = gK * (n0**4) * (tensao[0] - vK)
-pM = gNa * (m0**3) * h0 * (tensao[0] - vNa)
-pL = gL * (tensao[0] - vL)
-
-aux = tensao[0] - (pN + pM + pL) * gama
-tensao.append(aux)
-tempo.append(t0+dT)
-
-#print(tensao[1])
-
-M = [m0]
-M.append(m0)
-N = [n0]
-N.append(n0)
-H = [n0]
-H.append(h0)
-
-i = 0.0
-c = 1
-
-txt = input('Caso deseje alterar ∆t aparte S')
-if txt == 'S':
-    parada = float(input('Valor para tF em ms')) * 10**(-3) #ms 
-else:
-    parada = .25*10**(-3)
-
-while i <= parada:
-    auxN = dNdT(auxN, tensao[c])
-    N.append(auxN)
-    auxM = dMdT(auxM, tensao[c])
-    M.append(auxM)
-    auxH = dHdT(auxM, tensao[c])
-    H.append(auxH)
-
-    pN = gK * (auxN**4) * (tensao[c] - vK)
-    pM = gNa * (auxM**3) * auxH * (tensao[c] - vNa)
-    pL = gL * (tensao[c] - vL)
+    pN = gK * (n0**4) * (tensao[0] - vK)
+    pM = gNa * (m0**3) * h0 * (tensao[0] - vNa)
+    pL = gL * (tensao[0] - vL)
 
     aux = tensao[0] - (pN + pM + pL) * gama
     tensao.append(aux)
- 
-    c += 1
-    i = i + dT
-    tempo.append(i)
+    tempo.append(t0+dT)
 
-#print(tensao)
+    #print(tensao[1])
 
-matplotlib.pyplot.ioff()
-matplotlib.pyplot.plot(tempo,tensao,label='V(t) V')
-#matplotlib.pyplot.plot(tempo,N,label='N(t)')
-#matplotlib.pyplot.plot(tempo,M,label='M(t)')
-#matplotlib.pyplot.plot(tempo,H,label='H(t)')
-matplotlib.pyplot.xlabel('tempo (t) s')
-matplotlib.pyplot.ylabel('valores (V(t))')
-matplotlib.pyplot.title('Prova 2 Modelo H-H graph - Gustavo R. BEC')
-matplotlib.pyplot.legend()
-matplotlib.pyplot.show()
+    M = [m0]
+    M.append(m0)
+    N = [n0]
+    N.append(n0)
+    H = [n0]
+    H.append(h0)
+
+    i = 0.0
+    c = 1
+
+    txt = input('Caso deseje alterar ∆t aparte S')
+    if txt == 'S':
+        parada = float(input('Valor para tF em ms')) * 10**(-3) #ms 
+    else:
+        parada = .25*10**(-3)
+
+    while i <= parada:
+        auxN = dNdT(auxN, tensao[c])
+        N.append(auxN)
+        auxM = dMdT(auxM, tensao[c])
+        M.append(auxM)
+        auxH = dHdT(auxM, tensao[c])
+        H.append(auxH)
+
+        pN = gK * (auxN**4) * (tensao[c] - vK)
+        pM = gNa * (auxM**3) * auxH * (tensao[c] - vNa)
+        pL = gL * (tensao[c] - vL)
+
+        aux = tensao[0] - (pN + pM + pL) * gama
+        tensao.append(aux)
+     
+        c += 1
+        i = i + dT
+        tempo.append(i)
+
+    #print(tensao)
+
+    matplotlib.pyplot.ioff()
+    matplotlib.pyplot.plot(tempo,tensao,label='V(t) V')
+    #matplotlib.pyplot.plot(tempo,N,label='N(t)')
+    #matplotlib.pyplot.plot(tempo,M,label='M(t)')
+    #matplotlib.pyplot.plot(tempo,H,label='H(t)')
+    matplotlib.pyplot.xlabel('tempo (t) s')
+    matplotlib.pyplot.ylabel('valores (V(t))')
+    matplotlib.pyplot.title('Prova 2 Modelo H-H graph - Gustavo R. BEC')
+    matplotlib.pyplot.legend()
+    matplotlib.pyplot.show()
+
+    print('Rodar novamente? [S/N]')
+    again = input()
